512fef4a9b36a0e1d9ecf0a077ca39683aa06430
helenaperdigueiro <helena.perdigueiro@gmail.com>
2021-09-10 11:59:44 -0300

    mvp

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let scoreHelena = 0;
+document.getElementById("addUdemy").addEventListener("click", function() {
+    
+    document.getElementById("scoreHelena").innerHTML = ++scoreHelena;
+
+})
+
+let scoreWill = 0;
+document.getElementById("addDinamic").addEventListener("click", function() {
+    
+    document.getElementById("scoreWill").innerHTML = ++scoreWill;
+
+})
